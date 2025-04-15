ebdde945664d3628387bee02f918ea01286dc9fd
Ginger de Ridder <ginger_de_ridder@hotmail.com>
2025-04-15 20:25:52 +0800

    Quick commit

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,8 +1,9 @@
 // Import modularized components
-import { lineMargin, lineWidth, lineHeight, drawlineChart } from './lineChart.js';
-import { correlationMargin, correlationWidth, correlationHeight, updateCorrelationChart } from './correlationChart.js';
-import { tableMargin, tableWidth, tableHeight, updatePerformanceTable } from './performanceTable.js';
-import { wordBubbles } from './wordbubbles.js';
+import { lineMargin, lineWidth, lineHeight, drawlineChart } from './static/lineChart.js';
+import { correlationMargin, correlationWidth, correlationHeight, updateCorrelationChart } from './static/correlationChart.js';
+import { tableMargin, tableWidth, tableHeight, updatePerformanceTable } from './static/performanceTable.js';
+import { wordBubbles } from './static/wordbubbles.js';
+
 
 // Format date for display
 const formatDate = d3.timeFormat("%b %d, %Y");
